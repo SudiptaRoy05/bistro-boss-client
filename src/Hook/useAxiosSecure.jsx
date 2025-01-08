fbc3d4d4d4a5dd39c13237d65ef8019e116b4ca5
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2025-01-08 12:23:20 +0600

    logout unAuthorized user

--- src/Hook/useAxiosSecure.jsx
+++ src/Hook/useAxiosSecure.jsx
@@ -1,9 +1,33 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 export default function useAxiosSecure() {
+    const navigate = useNavigate();
+    const { logout } = useAuth();
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token');
+        console.log("URUHEBH FHIHJNSJJSH ")
+        config.headers.authorization = `bearer ${token}`
+        return config
+    }, function (error) {
+        return Promise.reject(error)
+    })
+
+    axiosSecure.interceptors.response.use(function (response) {
+        return response;
+    }, async (error) => {
+        const status = error.response.status
+        console.log("status error", status)
+        if (status === 401 || status === 403) {
+            await logout();
+            navigate('/login')
+        }
+        return Promise.reject(error);
+    })
     return axiosSecure;
 }
 
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,11 +1,13 @@
 import { createContext, useEffect, useState } from "react"
 import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../Firebase/firebase.config";
+import useAxiosPublic from "../Hook/useAxiosPublic";
 // import Login from "../Pages/Login/Login";
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 export default function AuthProvider({ children }) {
+    const axiosPublic = useAxiosPublic();
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true)
     const provider = new GoogleAuthProvider();
@@ -40,12 +42,26 @@ export default function AuthProvider({ children }) {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
             // console.log(currentUser)
+            if (currentUser) {
+                // get Token
+                const userInfo = { email: currentUser.email };
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        console.log(res.data.token)
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token);
+                        }
+                    })
+            }
+            else {
+                localStorage.removeItem('access-token')
+            }
             setLoading(false);
         });
         return () => {
             return unSubscribe();
         }
-    }, [])
+    }, [axiosPublic])
 
     const authInfo = {
         user,
